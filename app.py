055f71244e4e2e1ee4719cfbdea541ccd7cb9172
Nakiyah Dhariwala <nakiyahdhariwala@Nakiyahs-MacBook-Pro-2.local>
2024-12-09 15:58:32 +0530

    fixing app

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
-from mylib.functions import loadCSV, loadTeams, extractSeeds, extractFirstLastRanks, extractConferences
+from mylib.functions import loadTeams, extractSeeds, extractFirstLastRanks, extractConferences
 from mylib.functions import finalFour, teamStats
 from mylib.lib import read_s3_csv
-import boto3
 from datetime import datetime
 import pytz
-import os
+import boto3
 import logging
         
 # Initialize the Flask application
@@ -15,7 +14,6 @@ app = Flask(__name__)
 #     exec(f.read())
 # with open("final_model.py") as f: 
 #     exec(f.read())
-
 
 # Configure logging
 logging.basicConfig(
@@ -30,26 +28,18 @@ logging.basicConfig(
 # Log app start
 logging.info("Initializing March Madness Predictor Flask application.")
 
-# S3 Configuration
+# Load and process data
+
 s3 = boto3.client("s3")
 bucket_name = "cbb-data-engg"
 output_prefix = "Final_Project_DE/"
 current_date = datetime.now().strftime("%Y%m%d")
 file_name_final = f"cbb25_seeded_{current_date}.csv"
 
-logging.info(f"Loading data from {df}")
-df = read_s3_csv(bucket_name, f"{output_prefix}{file_name_final}")
-logging.info("Processing teams data with loadTeams.")
-dfClean = loadTeams(df)
-dfGroupSeed = extractSeeds(dfClean)
-dfFirstLastRanks = extractFirstLastRanks(dfClean)
-dfGroupConference = extractConferences(dfClean)
-logging.info("Data loaded and processed successfully.")
+
 
 # Page 2
-logging.info("Predicting Final Four results.")
-Top4 = finalFour(dfClean)  # Predict Final Four results
-logging.info(f"Final Four prediction completed: {Top4}")
+
 
 @app.route("/")
 def index():
@@ -57,6 +47,14 @@ def index():
         # Get current time in Eastern Time (ET)
         eastern = pytz.timezone("US/Eastern")
         last_updated = datetime.now(eastern).strftime("%Y-%m-%d %I:%M:%S %p")
+        
+        logging.info(f"Loading data from {output_prefix}{file_name_final}")
+        df = read_s3_csv(bucket_name, f"{output_prefix}{file_name_final}")
+        dfClean = loadTeams(df)
+        dfGroupSeed = extractSeeds(dfClean)
+        dfFirstLastRanks = extractFirstLastRanks(dfClean)
+        dfGroupConference = extractConferences(dfClean)
+        logging.info("Data loaded and processed successfully.")
         
         logging.info("Rendering index page.")
         
@@ -82,6 +80,11 @@ def index():
 @app.route("/final_four")
 def final_four():
     try:
+        df = read_s3_csv(bucket_name, f"{output_prefix}{file_name_final}")
+        dfClean = loadTeams(df)
+        logging.info("Predicting Final Four results.")
+        Top4 = finalFour(dfClean)  # Predict Final Four results
+        logging.info(f"Final Four prediction completed: {Top4}")
         logging.info("Rendering Final Four page.")
         team_stats = teamStats(df, Top4)
         return render_template(
