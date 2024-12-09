64411ab695c7516e01c8493e0d9d20ef57f4072f
Arko Bhattacharya <arko_bhattacharya@hotmail.com>
2024-12-09 04:07:29 -0500

    kaggle extract to csv and data pipeline

--- read_write_files_s3.py
+++ read_write_files_s3.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import logging
 import boto3
-from mylib.lib import upload_to_s3, read_s3_csv, write_s3_csv, extract_bart_torvik_data
+from mylib.lib import read_s3_csv, write_s3_csv, extract_bart_torvik_data
 
 logging.basicConfig(
-    filename="data_processing.log",
+    filename="logs/read_write_files_s3.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
@@ -15,31 +15,12 @@ bucket_name = "cbb-data-engg"
 input_prefix = "Final_Project_DE/archive/"
 output_prefix = "Final_Project_DE/"
 
+# Extract and write current live data
+logging.info("Extracting and saving current live data.")
 current_data = extract_bart_torvik_data()
 write_s3_csv(current_data, bucket_name, f"{output_prefix}current_cbb_live_data.csv")
 
-local_folder = "../Final_Project_DE/archive"
-files_to_upload = [
-    "cbb13.csv",
-    "cbb14.csv",
-    "cbb15.csv",
-    "cbb16.csv",
-    "cbb17.csv",
-    "cbb19.csv",
-    "cbb18.csv",
-    "cbb21.csv",
-    "cbb22.csv",
-    "cbb23.csv",
-    "cbb24.csv",
-    "cbb.csv",
-]
-
-for file_name in files_to_upload:
-    local_file_path = f"{local_folder}/{file_name}"
-    s3_key = f"{input_prefix}{file_name}"
-    upload_to_s3(local_file_path, bucket_name, s3_key)
-
-"""Process files stored in S3."""
+# Process files stored in S3
 logging.info("Processing cbb22.csv.")
 df_22 = read_s3_csv(bucket_name, f"{input_prefix}cbb22.csv")
 df_22.rename(columns={"EFGD_D": "EFG_D"}, inplace=True)
@@ -61,6 +42,7 @@ df_16.drop(
 
 write_s3_csv(df_16, bucket_name, f"{input_prefix}cbb16.csv")
 
+# Combine multiple CSV files
 logging.info("Combining multiple CSV files.")
 response = s3.list_objects_v2(Bucket=bucket_name, Prefix=input_prefix)
 csv_files = [
@@ -79,6 +61,7 @@ dataframes = [read_s3_csv(bucket_name, file_key) for file_key in csv_files]
 combined_df = pd.concat(dataframes, ignore_index=True)
 write_s3_csv(combined_df, bucket_name, f"{output_prefix}combined_cbb.csv")
 
+# Process cbb24.csv
 logging.info("Processing cbb24.csv.")
 cbb24 = read_s3_csv(bucket_name, f"{input_prefix}cbb24.csv")
 cbb24["EFG_O"] = cbb24["EFG%"]
@@ -112,6 +95,3 @@ cbb24 = cbb24[
 
 write_s3_csv(combined_df, bucket_name, f"{output_prefix}train_data.csv")
 write_s3_csv(cbb24, bucket_name, f"{output_prefix}test_data.csv")
-
-
-    
